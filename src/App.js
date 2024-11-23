65024a19c45f30d6bf7ed539bec9664130f9074b
Fahad4920 <fahadsajid492@gmail.com>
2024-11-23 13:19:50 +0500

    Setting App.js
    ...

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import Header from './components/Home/header';
-import Preloader from './components/Home/Preloader';
-import Welcome from './components/Home/index';
-import Services from './components/Home/Services-short';
-import OurWork from './components/Home/Our_Work';
-import Team from './components/Home/team';
-import Testimonials from './components/Home/Testimonials';
-import Contact from './components/Home/contact';
-import Footer from './components/Home/footer';
-import Navbar from './components/Home/NavBar';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home'; // Import the Home component
+import About from './components/AboutUs/About';
 
-
-function App() {
-  const [showPreloader, setShowPreloader] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowPreloader(false); // Hide Preloader after 2 seconds
-    }, 2000);
-
-    return () => clearTimeout(timer); // Cleanup timer on component unmount
-  }, []);
-
+const App = () => {
   return (
-    <>
-      {showPreloader ? <Preloader /> : null} {/* Render Preloader only if showPreloader is true */}
-      {!showPreloader && ( // Render other components after Preloader
-        <>
-        
-        <Navbar/>
-          <Welcome/>
-          <Services/>
-          <OurWork/>
-          <Team/>
-          <Testimonials/>
-          <Contact/>
-          <Footer/>
-        </>
-      )}
-    </>
+    <Router>
+      <Routes>
+        {/* Define routes for different components */}
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import Header from './header';
+import Preloader from './Preloader';
+import Welcome from './index';
+import Services from './Services-short';
+import OurWork from './Our_Work';
+import Team from './team';
+import Testimonials from './Testimonials';
+import Contact from './contact';
+import Footer from './footer';
+import Navbar from './NavBar';
+
+const Home = () => {
+  const [showPreloader, setShowPreloader] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowPreloader(false); // Hide Preloader after 2 seconds
+    }, 2000);
+
+    return () => clearTimeout(timer); // Cleanup timer on component unmount
+  }, []);
+
+  return (
+    <>
+      {showPreloader ? <Preloader /> : null} {/* Render Preloader only if showPreloader is true */}
+      {!showPreloader && ( // Render other components after Preloader
+        <>
+          <Navbar />
+          <Welcome />
+          <Services />
+          <OurWork />
+          <Team />
+          <Testimonials />
+          <Contact />
+          <Footer />
+        </>
+      )}
+    </>
+  );
+};
+
+export default Home;
