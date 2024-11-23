f73a742e963d6f88a1e7fee93b287b4d08b58cd5
Fahad4920 <fahadsajid492@gmail.com>
2024-11-23 13:26:09 +0500

    Setting App.js
    ...

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home'; // Import the Home component
 import About from './components/AboutUs/About';
+import Services from './components/Services/Services';
+import Team from './components/Team/Team';
+import Portfolio from './components/Portfolio/Portfolio';
+
 
 const App = () => {
   return (
@@ -10,6 +14,9 @@ const App = () => {
         {/* Define routes for different components */}
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="services" element={<Services />} />
+        <Route path="team" element={<Team />} />
+        <Route path="portfolio" element={<Portfolio />} />
       </Routes>
     </Router>
   );
