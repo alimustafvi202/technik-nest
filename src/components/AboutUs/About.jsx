e6ff64d88174d1f76946ecd1c478b1805d49b96d
Fahad4920 <fahadsajid492@gmail.com>
2024-11-25 12:10:13 +0500

    update
    ..............

--- src/components/AboutUs/About.jsx
+++ src/components/AboutUs/About.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./About.css";
-import Navbar from "../Home/NavBar";
 import Footer from "../Home/footer";
 import Contact from "../Home/contact";
 
--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
-import Navbar from '../Home/NavBar';
 import { AnimatePresence } from 'framer-motion';
 import AboutUs from '../AboutUs/About';
 import ServicesMain from '../Services/Services';
@@ -8,16 +7,16 @@ import PastWork from '../Portfolio/Portfolio';
 import HireUs from '../Hire-Us/Hire-Us';
 import Home from '../Home/Home';
 import Graphic from '../Home/Graphic/graphic';
+import Navbar from '../Home/NavBar';
 
 const AnimatedRoutes = () => {
   const location = useLocation();
 
   return (
     <>
-    
+    <Navbar />
     <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
-      <Navbar />
       <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path='/services' element={<ServicesMain/>}/>
